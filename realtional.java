d172b6b4b19c4afa611cd14236952b497ef5134f
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-01 12:34:33 +0530

    Create realtional.java

---
+++ realtional.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Relational
+{
+	public static void main(String args[])
+	{
+		int a;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values of a,b: ");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a==b);
+		System.out.println(a!=b);	
+}		
+}
