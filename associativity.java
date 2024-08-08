4a42f1b37be58825ce4e6982115147c9fd1e4346
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 12:02:13 +0530

    Create associativity.java

---
+++ associativity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Asso
+{
+	public static void main(String args[])
+	{
+		int a;
+		int b;
+		a=12*4-2;
+		b=12*4*2/2+1;
+		System.out.println("the value of a is "+a);
+		System.out.println("the value of b is "+b);
+		
+	}
+}		
