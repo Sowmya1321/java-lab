b2240d4c6a1c684241df07cce2cf5ab18b0fa8d4
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 12:19:05 +0530

    Create if(positive).java

---
+++ if(positive).java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Positive
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number: ");
+		int a= sc.nextInt();
+		if(a>0)
+		{
+			System.out.println(a+" is positive");
+		}
+	}
+}
