5b1591632ecb2be03c7af519c1f34647faf37335
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 12:24:38 +0530

    Create formatted output printf.java

---
+++ formatted output printf.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Specifiers
+{
+	public static void main(String args[])
+	{
+		int age = 23;
+		String name = "suneel";
+		double score = 95.2;
+		System.out.printf("%d,%s,%f",age,name,score);
+	}
+}
