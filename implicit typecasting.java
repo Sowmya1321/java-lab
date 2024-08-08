0042b23c29d29f3c8fa7164633a16d586dd41bcc
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 11:47:26 +0530

    Create implicit typecasting.java

---
+++ implicit typecasting.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Intdouble
+{
+	public static void main(String args[])
+	{
+		int num=10;
+		System.out.println("integer value is "+num);
+		double d = num; 
+		System.out.println("double values is "+d);
+	}
+}		
