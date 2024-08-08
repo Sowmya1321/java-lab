7bf03e07732b817b4f83a00b0f15d31fc652fe90
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 11:27:57 +0530

    Create total of array elements.java

---
+++ total of array elements.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Total
+{
+	public static void main(String args[])
+	{
+		int arr[]={10,11,12,13};
+		int total=0;
+		for(int i:arr)
+		{
+			total+=i;
+		}
+		System.out.println("the total is "+total);
+	}
+}			
+		
