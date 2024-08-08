278a83d54fb342fd568dddbd27f270214da8aace
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 12:30:31 +0530

    Create positive or negative(if else).java

---
+++ positive or negative(if else).java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Pos
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the number");
+		int a = sc.nextInt();
+		if(a>0)
+		{
+			System.out.println(a+" is positive");
+		}
+		else
+		{
+			System.out.println(a+" is negative");
+		}		
+	}
+}
