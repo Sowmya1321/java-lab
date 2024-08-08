b038c19ff9a148a8e555e9b01d4ef5bf099a937c
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 11:55:50 +0530

    Create explicit type conversion.java

---
+++ explicit type conversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Type
+{
+	public static void main(String args[])
+	{
+		double num = 10.69;
+		System.out.println("double value is "+num);
+		int a = (int)num;
+		System.out.println("integer value is "+a);
+		byte b = (byte)a;
+		System.out.println("byte value is "+b);
+	}
+}		
