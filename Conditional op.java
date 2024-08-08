fd351c83e21caf170df95798c4fa0f4efa791867
Sowmya1321 <23pa1a05f1@vishnu.edu.in>
2024-08-08 17:52:06 +0530

    Create Conditional op.java

---
+++ Conditional op.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Conditional
+{
+	public static void main(String args[])
+	{
+		int a;
+		int b;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a,b values:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		int Max = (a>b)?a:b;
+		System.out.println("maximum");
+	}
+}
+
+
+
+
+
